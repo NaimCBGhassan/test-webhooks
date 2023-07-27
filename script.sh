15f84d324bd867353e779703e2113a1d4b3f78c5
Naim Chaya <naimchaya.dev@gmail.com>
2023-07-27 13:06:10 -0300

    5

--- script.sh
+++ script.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 # Detener los servicios Docker existentes
 cd /home/ubuntu/Veterinaria/ && /usr/bin/docker-compose down 
+cd /home/ubuntu/Veterinaria/ && /usr/bin/docker image prune -a
 #<<< "TU_CONTRASEÑA_DE_SUDO"
 # Configuración de usuario y contraseña de Git
 export GIT_USERNAME="jpsq"
 export GIT_PASSWORD="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"
 # Configurar el nombre de usuario de Git globalmente
-/usr/bin/git config --global user.name "$GIT_USERNAME"
+git config --global user.name "$GIT_USERNAME"
 # Configurar la contraseña de Git globalmente
-/usr/bin/git config --global credential.helper "store --file ~/.git-credentials"
+git config --global credential.helper "store --file ~/.git-credentials"
 echo -e "https://$GIT_USERNAME:$GIT_PASSWORD@github.com" > ~/.git-credentials
 # Actualizar el repositorio git
-cd /home/ubuntu/Veterinaria/ && /usr/bin/git pull origin test 
+cd /home/ubuntu/Veterinaria/ && git pull origin test 
 # Detener los servicios Docker nuevamente (por si acaso)
 cd /home/ubuntu/Veterinaria/ && /usr/bin/docker-compose down
 #<<< "TU_CONTRASEÑA_DE_SUDO"
